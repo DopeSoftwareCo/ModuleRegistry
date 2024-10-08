91c16647b6919be1f8db2bf347ed9da4e541ea7b
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-10-08 13:42:53 -0400

    add package controller created, types completed

--- BE/src/Types/ResponseTypes/index.d.ts
+++ BE/src/Types/ResponseTypes/index.d.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import { PackageData, PackageHistoryEntry, PackageMetaData, PackageRating, User } from "../Models";
 
 declare module "ResponseTypes" {
     export type GetPackagesInvalidResponseMessages =
@@ -7,11 +8,7 @@ declare module "ResponseTypes" {
 
     export type GetPackagesResponseBody =
         | {
-              packages: {
-                  Version: string;
-                  Name: string;
-                  ID: string;
-              }[];
+              packages: PackageMetaData[];
           }
         | GetPackagesInvalidResponseMessages;
 
@@ -35,15 +32,8 @@ declare module "ResponseTypes" {
         | "Package does not exist.";
 
     export type GetPackageViaIDResponseBody = {
-        metadata: {
-            Name: string;
-            Version: string;
-            ID: string;
-        };
-        data: {
-            content: string;
-            JSProgram: string;
-        };
+        metadata: PackageMetadataResponse;
+        data: PackageData;
     };
 
     export interface GetPackageViaIDResponse extends Response {
@@ -60,14 +50,58 @@ declare module "ResponseTypes" {
         | "There is missing field(s) in the PackageID/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid."
         | ("Package does not exist." & Response);
 
-    export type UploadInjectResponseMessages =
+    export type UploadInjestResponseMessages =
         | "There is missing field(s) in the PackageData/AuthenticationToken or it is formed improperly (e.g. Content and URL are both set), or the AuthenticationToken is invalid."
         | "Package exists already."
         | "Pacakge is not uploaded due to disqualified rating.";
 
-    export type UploadInjectNewPackageResponseBody = GetPackageViaIDResponseBody;
+    export type UploadInjestNewPackageResponseBody = {
+        metadata: PackageMetadataResponse;
+        data: PackageData;
+    };
 
-    export interface UploadInjectNewPackageResponse extends Response {
-        body: UploadInjectNewPackageBody | UploadInjectResponseMessages;
+    export interface UploadInjestNewPackageResponse extends Response {
+        body: UploadInjectNewPackageBody;
+    }
+
+    export type GetRatingsForPackageResponseBody = PackageRating;
+
+    export type GetRatingsForPackageInvalidResponses =
+        | "There is missing field(s) in the PackageID/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid."
+        | "Package does not exist."
+        | "The package rating system choked on at least one of the metrics.";
+
+    export interface GetRatingsForPackageResponse extends Response {
+        body: GetRatingsForPackageResponseBody;
+    }
+
+    //the valid response is a bearer token for the FE, that is missing here.
+    export type AuthenticateInvalidResponses =
+        | "There are missing field(s) in the AuthenticationRequest or it is formed improperly."
+        | "The user or password is invalid."
+        | "The system does not support authentication.";
+
+    export type GetHistoryOfPackageResponseBody = PackageHistoryEntry;
+
+    export type GetHistoryOfPackageResponseMessages =
+        | "There is missing field(s) in the PackageName/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid."
+        | "No such package";
+
+    export interface GetHistoryOfPackageRequest extends Response {
+        body: GetHistoryOfPackageResponseBody;
+    }
+
+    export type DeletePackageByNameResponse =
+        | "Package is deleted."
+        | "There is missing field(s) in the PackageName/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid."
+        | "Package does not exist.";
+
+    export type GetPackageViaRegexInvalidResponseMessages =
+        | "There is missing field(S) in the PackageRegEx/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid."
+        | "No package found under this regex.";
+
+    export type GetPackageViaRegexData = Omit<PackageMetaData, "ID">;
+    export interface GetPackageViaRegexResponse extends Response {
+        body: GetPackageViaRegexData[];
     }
 }
