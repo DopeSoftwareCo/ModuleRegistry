b181df1ccaad7737f3da6f19ba8c97ad407648f8
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-10-10 20:55:06 -0400

    fixed import bugs

--- BE/src/Routes/ModulesRoute.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { ModuleValidationRules } from "../Validation/ModuleValidation/ModuleValidationRules";
-import { validateRequest } from "../Validation/validator";
-import { AddModuleController } from "../Controllers/Module/ModulesController";
-
-export const ModuleRouter = Router();
-
-ModuleRouter.post("/add", ModuleValidationRules, validateRequest, AddModuleController);
