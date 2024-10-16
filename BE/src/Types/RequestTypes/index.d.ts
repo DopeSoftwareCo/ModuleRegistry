953973d3ca767adba6bec43719e2bb7ea66224a2
Dorian Bell II <MsDorianBell@outlook.com>
2024-10-16 10:37:55 -0400

    Restored ReqTypes, RespTypes to Ben's last commit

--- BE/src/Types/RequestTypes/index.d.ts
+++ BE/src/Types/RequestTypes/index.d.ts
@@ -10,13 +10,52 @@ declare module "RequestTypes" {
         body: TestRequestBody;
     }
 
-    export type AddModuleRequestBody = {
-        url: string;
-        repoName?: string;
-        repoOwner?: string;
+    export type GetPackagesData = Omit<PackageMetaData, "ID">;
+
+    export interface GetPackagesRequest extends Request {
+        body: GetPackagesData[];
+    }
+
+    export interface ResetRegistryRequest extends Request {}
+
+    export interface GetPackageViaIdRequest extends Request {}
+
+    export type UpdatePackageContentRequestBody = {
+        metadata: PackageMetaData;
+        data: PackageData;
     };
 
-    export interface AddModuleRequest extends Request {
-        body: AddModuleRequestBody;
+    export interface UpdatePackageContentRequest extends Request {
+        body: UpdatePackageContentRequestBody;
     }
+
+    export interface DeletePackageByIDRequest extends Request {}
+
+    export type UploadInjestPackageRequestBody = PackageData;
+
+    export interface UploadInjestPackageRequest extends Request {
+        body: UploadInjestPackageRequestBody;
+    }
+
+    export interface GetPackageRatingsRequest extends Request {}
+
+    export type AuthenticationRequestBody = AuthenticationRequestModel;
+
+    export interface AuthenticationRequest extends Request {
+        body: AuthenticationRequestBody;
+    }
+
+    export interface GetHistoryOfPackageByNameRequest extends Request {}
+
+    export interface DeleteAllVersionsByNameRequest extends Request {}
+
+    export type GetPackagesViaRegexRequestBody = {
+        RegEx: string;
+    };
+
+    export interface GetPackagesViaRegexRequest extends Request {
+        body: GetPackagesViaRegexRequestBody;
+    }
+
+    export interface GetPackageByNameRequest extends Request {}
 }
