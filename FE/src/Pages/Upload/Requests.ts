c19ec9951ddea2a1ef89d1e775dce579d98ed672
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-10-29 16:49:16 -0400

    removed commented out request

--- FE/src/Pages/Upload/Requests.ts
+++ FE/src/Pages/Upload/Requests.ts
@@ -38,34 +38,3 @@ export const uploadFile = async (
         } else errorSetter('Some unknown error occured in uploadFile.');
     }
 };
-
-/**
- * 
- * 
- * export const getPackagesViaRegexRequest = async (
-    regex: string,
-    errorSetter: (error: string) => void
-): Promise<RegexPackagesFromAPI | undefined> => {
-    try {
-        const response = await fetch(`${GeneralConfig.BACKEND_URL}package/byRegEx`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `${localStorage.getItem('token')}`,
-            },
-            body: JSON.stringify({ RegEx: regex }),
-        });
-        if (!response.ok) {
-            const responseText = await response.text();
-            errorSetter(responseText);
-            return undefined;
-        }
-        const regexPackages: RegexPackagesFromAPI = await response.json();
-        return regexPackages;
-    } catch (err) {
-        if (err instanceof Error) {
-            errorSetter(err.message);
-        } else errorSetter('an unknown error occured in getPackagesViaRegexRequest');
-    }
-};
- */
