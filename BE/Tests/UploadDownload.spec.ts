276dd6a624af8265e50fce938f0b52b122f0b0e2
Ben Kanter <benkanter@live.com>
2024-11-03 22:00:18 -0500

    wrote further code

--- BE/Tests/UploadDownload.spec.ts
+++ BE/Tests/UploadDownload.spec.ts
@@ -4,6 +4,13 @@ import { UploadInjestPackageRequest } from "../src/Types/RequestTypes/index.d";
 import { UploadInjestNewPackageResponse } from "../src/Types/ResponseTypes/index.d";
 import { NextFunction } from "express";
 
+const urls_A = [
+    "https://www.npmjs.com/package/typescript",
+    "https://github.com/msys2/MINGW-packages",
+    "https://www.npmjs.com/package/@docsearch/js",
+    "https://github.com/orangeduck/mpc",
+];
+
 describe("Version Dependence", () => {
     test("Download Our Repo", async () => {
         const request: Partial<UploadI> = {
--- BE/src/Controllers/UploadInjestControllers.ts
+++ BE/src/Controllers/UploadInjestControllers.ts
@@ -12,6 +12,10 @@ import { debloatUploadedContent } from "../DSinc_Modules/DSinc_PackageHandling";
 import fs from 'fs';
 import axios from 'axios';
 import path from 'path';
+import unzipper, { Entry } from 'unzipper'
+import { ModuleEvaluator } from "../Providers/ModEval/ModuleEvaluator";
+import { DEFAULT_WEIGHTS } from "../Providers/ModEval/RepoComponents/Metrics_Scores/Weightspec.const";
+import { SuperRepoBuilder } from "../Providers/ModEval/RepoComponents/Builders/SuperRepoBuilder";
 
 
 export const UploadInjestController = asyncHandler(
@@ -29,6 +33,7 @@ export const UploadInjestController = asyncHandler(
         const tempIDCeiling = 1000;
         const tempID = (Math.floor((Math.random() * tempIDCeiling) + 1)).toString();
         const tempFile = path.join(tempDirectory, tempID)
+        const tempUnzippedFileDirectory = path.join(tempDirectory, tempID);
         if (!fs.existsSync(tempDirectory)) { // This is where data is downloaded before being examined. 
             fs.mkdirSync(tempDirectory);
         }
@@ -57,9 +62,17 @@ export const UploadInjestController = asyncHandler(
         }
 
         fs.promises.writeFile(tempFile, binaryContent);
+        const zipStream = unzipper.Parse();
+        zipStream.on('entry', async (entry: Entry) => {
+            const individualFilePath = path.join(tempUnzippedFileDirectory, entry.path);
+            const writeStream = fs.createWriteStream(individualFilePath);
+            entry.pipe(writeStream);
+        });
+        const packageJsonFile = await fs.promises.readFile(path.join(tempUnzippedFileDirectory, 'package.json'), 'utf-8');
+        const packageJson = JSON.parse(packageJsonFile);
 
         if (getRepoURL) {
-            repositoryUrl = "https://github.com/DopeSoftwareCo/ModuleRegistry"; // PLACEHOLDER, properly assigns it.
+            repositoryUrl = packageJson.homepage as string;
         }
 
         // Checks if exists
@@ -76,16 +89,36 @@ export const UploadInjestController = asyncHandler(
                 res.status(424).send(responseMessage);
             }
         }
+
+        const evaluator = new ModuleEvaluator(DEFAULT_WEIGHTS);
+        const repoBuilder = new SuperRepoBuilder();
+        const repository = await repoBuilder.SuperBuild(repositoryUrl);
+        if (repository == undefined) {
+            responseMessage = "Package is not uploaded due to disqualified rating.";
+            res.status(424).send(responseMessage);
+            return;
+        }
+        const score = await evaluator.Eval(repository);
+
+
+        const package = new PackageModel(); // Placeholder
+        const savedpackage = await package.save(); // Placeholder
+        const packageID = savedpackage._id.toString()
+
         if (body.debloat == true) {
-            binaryContent = await debloatUploadedContent(binaryContent.toString('binary'));
+            binaryContent = await debloatUploadedContent(binaryContent.toString('binary')); // Placeholder
+            // Zip up, and store
+        }
+        else {
+            // Just move the existing zip to Data and rename to the ID.
         }
 
         // PackageModel.create(); // Need to get ID from this
         const returnBody: UploadInjestNewPackageResponseBody = {
             metadata: {
-                Name: "Name",
-                Version: "1.0.0", // Since this is an initial release
-                ID: "Some id", // Pull from Package
+                Name: packageJson.name,
+                Version: packageJson.version,
+                ID: packageID,
             },
             //all fields are optional in data
             data: {},
