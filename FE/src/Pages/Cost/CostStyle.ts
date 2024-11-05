4404e69cd14cf7804da1095b95248d8098b60bdd
Jorge <jorgepuga1779@gmail.com>
2024-11-04 21:12:17 -0500

    Updated for Consistency

--- FE/src/Pages/Cost/CostStyle.ts
+++ FE/src/Pages/Cost/CostStyle.ts
@@ -6,27 +6,20 @@ export const Inputs = styled(StyledBaseDiv)`
     gap: 1rem;
 `;
 
-// The ID container will have vertical alignment since the checkbox will be below it.
-export const IDContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 0.25rem;
-`;
+export const IDContainer = styled(StyledBaseDiv)``;
 
 export const CostIDInput = styled(StyledBaseTextInput)``;
 
-export const CheckboxContainer = styled.div`
-    display: flex;
-    align-items: center;
-`;
+export const CheckboxContainer = styled(StyledBaseDiv)``;
 
-// Modify the size a bit to match
+// Modify the size a bit to match the size of the ID container
 export const CostRequestButton = styled(StyledBaseButton)`
     height: 100%;
     min-height: 35px;
 `;
 
 export const CostLabel = styled(StyledBaseDiv)``;
+
 export const CostValue = styled(StyledBaseDiv)``;
 
 export const ErrorMessage = styled(StyledBaseDiv)`
