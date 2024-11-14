d1ba81eb54bfced8a1d92db37ee5ea0b640d79c8
Ben Kanter <benkanter@live.com>
2024-11-13 21:09:49 -0500

    Wrote the archive variant

--- BE/src/DSinc_Modules/DSinc_PackageHandling.ts
+++ BE/src/DSinc_Modules/DSinc_PackageHandling.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import axios from 'axios';
 import path from 'path';
 import * as unzipper from 'unzipper';
-//import * as jszip from 'jszip';
+import archiver from 'archiver'
 /**
  * @author Ben Kanter
  * @param content package content encoded in binary
@@ -54,6 +54,17 @@ export async function debloatUnzippedContent(folderPath: string): Promise<boolea
 const tempUnzippedDirectory = path.join(process.cwd(), "Data/Packages/.Temp/Debloat_Operations");
 export async function debloatZippedContent(zipPath: string): Promise<boolean> {
     let isSuccessful = true;
+    let isZip: boolean = false;
+    let isTarGz: boolean = false;
+    if (zipPath.endsWith(".zip")) {
+        isZip = true;
+    }
+    else if (zipPath.endsWith(".tar.gz")) {
+        isTarGz = true;
+    }
+    else {
+        return false; // Not a supported zip archive
+    }
     const zippedContent = await fs.promises.readFile(zipPath);
     const tempIDCeiling = 1000;
     const tempID = (Math.floor((Math.random() * tempIDCeiling) + 1)).toString();
@@ -61,7 +72,39 @@ export async function debloatZippedContent(zipPath: string): Promise<boolean> {
     await unzipper.Open.buffer(zippedContent).then((directory) => directory.extract ({ path: tempUnzippedFileDirectory}));
     isSuccessful = await debloatUnzippedContent(tempUnzippedFileDirectory);
     if (isSuccessful) {
-        // Zip up and send back
+        const backupZipPath = ("_" + zipPath); // Purpose of this is to provide a failsafe in case zipping operation fails
+        await fs.promises.rename(zipPath, backupZipPath);
+        try {
+            const output = fs.createWriteStream(zipPath);
+            let archive; 
+            if (isZip) {
+                archive = archiver('zip', { zlib: {level: 9} });
+            }
+            else if (isTarGz) {
+                archive = archiver('tar', { gzip: true, gzipOptions: { level: 9 } });
+            }
+            else { // Something went wrong in an unexpected way.
+                throw new Error("Neither a zip or tar, should not get to this point") // Gets caught and logged
+            }
+            output.on('close', () => {
+                return true;
+            });
+            archive.on('error', (error) => {
+                throw error; // Gets caught and logged
+            });
+            archive.pipe(output);
+            archive.directory(tempUnzippedDirectory, false);
+            archive.finalize();
+        }
+        catch (error){
+            console.error(error);
+            if (fs.existsSync(zipPath)) {
+                await fs.promises.rm(zipPath); // Removes in case there is some file there
+            }
+            await fs.promises.rename(backupZipPath, backupZipPath.slice(1)); // Revert
+            return false;
+        }
+        return true;
     }
     else {
         fs.promises.rm(tempUnzippedDirectory, { recursive: true, force: true}); // Removes the temp directory
