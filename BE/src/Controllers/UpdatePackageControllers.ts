32d3923ccf80916192b57c16edf1d1039845506a
TimCarpenter4250 <timbocarp03@gmail.com>
2024-11-19 20:55:39 -0500

    Implemented updating packages?

--- BE/src/Controllers/UpdatePackageControllers.ts
+++ BE/src/Controllers/UpdatePackageControllers.ts
@@ -3,6 +3,9 @@ import asyncHandler from "../Middleware/asyncHandler";
 import { UpdatePackageViaIDResponse, UpdatePackageViaIDResponseMessages } from "ResponseTypes";
 import { NextFunction } from "express";
 import PackageModel from "../Schemas/Package";
+import * as fs from "fs";
+import * as path from "path";
+import { debloatZippedContent, debloatUploadedContent } from "../DSinc_Modules/DSinc_PackageHandling";
 
 // /package/{id}
 export const UpdatePackageViaIDController = asyncHandler(
@@ -14,29 +17,88 @@ export const UpdatePackageViaIDController = asyncHandler(
 
         const pack = await PackageModel.findById(packageIDToUpdate);
 
-        let DNE;
+        let responseMessage: UpdatePackageViaIDResponseMessages;
 
-        if (pack) {
-            pack.data.Content = newData.data.Content!;
-            pack.data.JSProgram = newData.data.JSProgram!;
-            pack.repoUrl = newData.data.URL!;
-            pack.metadata.Name = newData.metadata.Name;
-            pack.metadata.Version = newData.metadata.Version;
-            pack.save();
-            DNE = false;
-        } else {
-            DNE = true;
+        if (pack?.metadata.Name == undefined) {
+            responseMessage = "Package does not exist.";
+            res.status(404).send(responseMessage);
         }
+        // console.log("VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV\n");
+        // console.log(packageIDToUpdate);
+        // console.log("\n");
+        // console.log(pack?.metadata.Name);
+        // console.log("\n");
+        // console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+
+        const body = req.body;
+        let repositoryUrl = body?.data.URL;
+        let content = body?.data.Content;
+        let binaryContent; // Meant to store the non-string encoded version
+        let getRepoURL = false;
+
+        const archiver = require("archiver");
+        const packagesDirectory = path.join(process.cwd(), "Data/Packages");
+        const tempDirectory = packagesDirectory + "/.Temp";
+        const tempFile = path.join(tempDirectory, packageIDToUpdate!);
+
+        if (!fs.existsSync(tempDirectory)) {
+            // This is where data is downloaded before being examined.
+            fs.mkdirSync(tempDirectory);
+        } else {
+            fs.rmSync(tempDirectory, { recursive: true, force: true });
+            fs.mkdirSync(tempDirectory);
+        }
+
+        if (content != undefined) {
+            // Confirmed that content exists, decode and extract repository URL.
+            binaryContent = Buffer.from(content, "base64");
+            repositoryUrl = repositoryUrl as unknown as string; // Type casts it from "string | undefined" to "string"
+            getRepoURL = true;
+        } else {
+            responseMessage =
+                "There is missing field(s) in the PackageID/AuthenticationToken or it is formed improperly, or the AuthenticationToken is invalid.";
+            res.status(424).send(responseMessage);
+            return;
+        }
+
+        await fs.promises.writeFile(tempFile, binaryContent);
+
+        if (body.data.debloat == true) {
+            // Zip up, and store
+            binaryContent = await debloatZippedContent(binaryContent.toString("binary")); // Placeholder
+            const zippedContentStream = fs.createWriteStream(
+                path.join(packagesDirectory, packageIDToUpdate!)
+            );
+            const archive = archiver("zip", { zlib: { level: 9 } });
+            archive.pipe(zippedContentStream);
+            archive.append(binaryContent);
+            archive.finalize();
+        } else {
+            // Just move the existing zip to Data and rename to the ID.
+            await fs.promises.copyFile(tempFile, path.join(packagesDirectory, packageIDToUpdate!));
+        }
+
+        // const returnBody: UpdatePackageViaIDResponse = {
+        //     metadata: {
+        //         Name: packageJson.name,
+        //         Version: packageJson.version,
+        //         ID: packageID,
+        //     },
+        //     //all fields are optional in data
+        //     data: {},
+        // }
+        pack!.data.Content = newData.data.Content!;
+        pack!.data.JSProgram = newData.data.JSProgram!;
+        pack!.repoUrl = newData.data.URL!;
+        pack!.metadata.Name = newData.metadata.Name;
+        pack!.metadata.Version = newData.metadata.Version;
+        pack!.save();
 
         //something to signify the package didnt exist
-        let responseMesasge: UpdatePackageViaIDResponseMessages;
+        // let responseMessage: UpdatePackageViaIDResponseMessages;
 
-        if (!DNE) {
-            responseMesasge = "Version is updated.";
-            res.status(200).send(responseMesasge);
-        } else {
-            responseMesasge = "Package does not exist.";
-            res.status(404).send(responseMesasge);
-        }
+        fs.rmSync(tempDirectory, { recursive: true, force: true });
+        responseMessage = "Version is updated.";
+        res.status(200).send(responseMessage);
     }
 );
