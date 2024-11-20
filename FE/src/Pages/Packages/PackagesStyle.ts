7843b9d70b4f0b1f0f2a95f70f22c8f50af64872
Jorge <jorgepuga1779@gmail.com>
2024-11-19 19:35:56 -0500

    added tentative styles

---
+++ FE/src/Pages/Packages/PackagesStyle.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { StyledBaseButton, StyledBaseDiv, StyledBaseTextInput } from '../../BaseStyledComponents/BaseStyled';
+
+export const PackagesInputs = styled(StyledBaseDiv)`
+    display: flex;
+    gap: 1rem;
+`;
+
+export const PackagesInput = styled(StyledBaseTextInput)``;
+
+export const PackagesRequestButton = styled(StyledBaseButton)``;
+
+export const PackagesResultName = styled(StyledBaseDiv)``;
+
+export const PackagesResultVersion = styled(StyledBaseDiv)``;
