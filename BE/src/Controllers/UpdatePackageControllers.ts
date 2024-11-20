3b3fe5ad7ac0c16e7860d0ce42b594ea6a644cea
TimCarpenter4250 <timbocarp03@gmail.com>
2024-11-20 16:24:54 -0500

    Now stores as .zip

--- BE/src/Controllers/UpdatePackageControllers.ts
+++ BE/src/Controllers/UpdatePackageControllers.ts
@@ -5,7 +5,7 @@ import { NextFunction } from "express";
 import PackageModel from "../Schemas/Package";
 import * as fs from "fs";
 import * as path from "path";
-import { debloatZippedContent, debloatUploadedContent } from "../DSinc_Modules/DSinc_PackageHandling";
+import { debloatZippedContent } from "../DSinc_Modules/DSinc_PackageHandling";
 
 // /package/{id}
 export const UpdatePackageViaIDController = asyncHandler(
@@ -22,21 +22,13 @@ export const UpdatePackageViaIDController = asyncHandler(
         if (pack?.metadata.Name == undefined) {
             responseMessage = "Package does not exist.";
             res.status(404).send(responseMessage);
+            return;
         }
-        // console.log("VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV\n");
-        // console.log(packageIDToUpdate);
-        // console.log("\n");
-        // console.log(pack?.metadata.Name);
-        // console.log("\n");
-        // console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
 
         const body = req.body;
-        let repositoryUrl = body?.data.URL;
         let content = body.data.Content;
         let binaryContent; // Meant to store the non-string encoded version
-        let getRepoURL = false;
 
-        const archiver = require("archiver");
         const packagesDirectory = path.join(process.cwd(), "Data/Packages");
         const tempDirectory = packagesDirectory + "/.Temp";
         const tempFile = path.join(tempDirectory, packageIDToUpdate!);
@@ -50,10 +42,7 @@ export const UpdatePackageViaIDController = asyncHandler(
         }
 
         if (content != undefined) {
-            // Confirmed that content exists, decode and extract repository URL.
-            repositoryUrl = repositoryUrl as unknown as string; // Type casts it from "string | undefined" to "string"
-            getRepoURL = true;
-            const base64Data = content.split(",")[1]; // Added this, basically removes the header
+            const base64Data = content.split(",")[1]; // Remove the file header
             binaryContent = Buffer.from(base64Data, "base64");
         } else {
             responseMessage =
@@ -65,21 +54,10 @@ export const UpdatePackageViaIDController = asyncHandler(
         await fs.promises.writeFile(tempFile, binaryContent);
 
         if (body.data.debloat == true) {
-            await debloatZippedContent(tempFile); // Returns whether or not successful, but if it is not successful, it just returns the original package *unless it hits an uncaught error*
-            // Removed the zipping section, function already does that :)
+            console.log(await debloatZippedContent(tempFile));
         }
-        // Removed Else Statement since it needs to copy regardless of debloat
-        // Just move the existing zip to Data and rename to the ID.
 
-        // const returnBody: UpdatePackageViaIDResponse = {
-        //     metadata: {
-        //         Name: packageJson.name,
-        //         Version: packageJson.version,
-        //         ID: packageID,
-        //     },
-        //     //all fields are optional in data
-        //     data: {},
-        // }
+        // Save to database
         pack!.data.Content = newData.data.Content!;
         pack!.data.JSProgram = newData.data.JSProgram!;
         pack!.repoUrl = newData.data.URL!;
@@ -87,12 +65,9 @@ export const UpdatePackageViaIDController = asyncHandler(
         pack!.metadata.Version = newData.metadata.Version;
         pack!.save();
 
-        // Moved this to after the database save incase errors happen, there won't be a mismatch between what the database says and what is stored.
-        await fs.promises.rename(tempFile, path.join(packagesDirectory, packageIDToUpdate!)); // Moves the file instead of copying.
-        //something to signify the package didnt exist
-        // let responseMessage: UpdatePackageViaIDResponseMessages;
+        // Save file
+        await fs.promises.rename(tempFile, path.join(packagesDirectory, packageIDToUpdate!) + ".zip");
 
-        //fs.rmSync(tempDirectory, { recursive: true, force: true }); // since file is moved, no longer needed
         responseMessage = "Version is updated.";
         res.status(200).send(responseMessage);
     }
