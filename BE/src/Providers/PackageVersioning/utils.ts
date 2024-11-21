c87ff2ae1b9f967c5ff2c04eb8aa888abc4d0ba7
Dorian Bell II <MsDorianBell@outlook.com>
2024-11-21 15:08:34 -0500

    restored feature files

---
+++ BE/src/Providers/PackageVersioning/utils.ts
@@ -0,0 +1,67 @@
+import { UpdateType, VersionRangeEndpoints } from "./types";
+
+export const versionFormat = "(\\d+)\\.(\\d+)\\.(\\d+)"; // Matches three groups of numbers.
+export const versionFormatRegex = `^[~^]?${versionFormat}(-${versionFormat})?$`; // Allows optional prefix and range.
+export const VersionType_RegExp = new RegExp(versionFormatRegex);
+export const SimpleVersion_RegExp = new RegExp(versionFormat);
+
+export function IsVersionString(someString: string): boolean {
+    return VersionType_RegExp.test(someString);
+}
+
+export function IncrementVersion(current: string, updateType: UpdateType): string | undefined {
+    // MajorStr.MinorStr.PatchStr <==> major#.minor#.patch#
+    const tokens = TokenizeVersion(current);
+    if (!tokens) {
+        return undefined;
+    }
+
+    let newVersion: string | undefined;
+    const dotZero = ".0";
+
+    let major = parseInt(tokens.major);
+    let minor = parseInt(tokens.minor);
+    let patch = parseInt(tokens.patch);
+
+    if (!major || !minor || !patch) {
+        return undefined;
+    }
+
+    if (updateType == UpdateType.Major) {
+        ++major;
+        minor = 0;
+        patch = 0;
+    } else if (updateType == UpdateType.Minor) {
+        ++minor;
+        patch = 0;
+    } else {
+        ++patch;
+    }
+
+    return `${major}.${minor}.${patch}`;
+}
+
+export function GetRangeEndpoints(dashFormattedRangeString: string): VersionRangeEndpoints | undefined {
+    if (dashFormattedRangeString.length < 1) {
+        return undefined;
+    }
+    const vString = dashFormattedRangeString.trim();
+    const components = vString.split("-");
+
+    if (components.length !== 2) {
+        return undefined;
+    }
+
+    const earliest = components[0];
+    const latest = components[1];
+    return SimpleVersion_RegExp.test(earliest) && SimpleVersion_RegExp.test(latest)
+        ? { oldest: earliest, newest: latest }
+        : undefined;
+}
+
+export function TokenizeVersion(
+    version: string
+): { major: string; minor: string; patch: string } | undefined {
+    const tokens = version.split(".");
+    return tokens.length === 3 ? { major: tokens[0], minor: tokens[1], patch: tokens[2] } : undefined;
+}
