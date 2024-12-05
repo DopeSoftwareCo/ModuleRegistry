7ca317bd59872c4a680e8a44f0c367798e081a21
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-12-05 12:10:28 -0500

    schema complete, created two tests

---
+++ BE/Tests/GQL.spec.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from "@jest/globals";
+import {
+    RepoQueryBuilder,
+    SendRequestToGQL,
+} from "../src/Providers/RepoEvaluator/GQL_Queries/Requests/GQLRequests";
+import { RepoID } from "../src/Providers/RepoEvaluator/RepoComponents/ID/RepoID";
+import { RepoID_Builder } from "../src/Providers/RepoEvaluator/RepoComponents/ID/RepoID_Builder";
+import { getFetchSpy } from "./TestUtils/mocks";
+
+describe("GQL", () => {
+    it("Should create a query string", async () => {
+        const b = new RepoID_Builder();
+        const repoId: RepoID | undefined = await b.Build("https://github.com/facebook/react");
+        const query = RepoQueryBuilder(repoId ? [repoId] : []);
+        expect(query).toBeDefined();
+    });
+
+    it("Should make request", async () => {
+        const s = getFetchSpy({ res: "string" }, 200);
+        const res = await SendRequestToGQL("");
+        expect(res).toEqual({ res: "string" });
+    });
+});
