4dcc0d2f8b837f0e7b6f8012aff13561794919c9
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-12-07 12:54:09 -0500

    maybe

--- BE/src/Validation/validator.ts
+++ BE/src/Validation/validator.ts
@@ -23,7 +23,11 @@ export const validateRequest = (req: Request, res: Response, next: NextFunction)
         //we are only returning one validation error message... so no matter the number ofmessages we can return the first if it exists
         const errsStrArr = result.array();
         if (errsStrArr.length > 0) {
-            return res.status(400).send(errsStrArr[0].msg);
+            if (errsStrArr[0].msg === "Package does not exist.") {
+                return res.status(404).send(errsStrArr[0].msg);
+            } else {
+                return res.status(400).send(errsStrArr[0].msg);
+            }
         }
     }
     return next();
