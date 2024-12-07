b26d9985ec82190bed815245916cd26bb30c5c72
j-leidy <71462506+j-leidy@users.noreply.github.com>
2024-12-06 22:56:27 -0500

    validation?

--- BE/src/Validation/PackageValidationRules/UpdateRules.ts
+++ BE/src/Validation/PackageValidationRules/UpdateRules.ts
@@ -20,11 +20,7 @@ export const UpdatePackageRules = [
         .withMessage(InvalidUploadPackageMessage)
         .isString()
         .withMessage(InvalidUploadPackageMessage),
-    body("data.debloat")
-        .exists()
-        .withMessage(InvalidUploadPackageMessage)
-        .isBoolean()
-        .withMessage(InvalidUploadPackageMessage),
+    body("data.debloat").optional().isBoolean().withMessage(InvalidUploadPackageMessage),
     body("data.URL")
         .exists()
         .withMessage(InvalidUploadPackageMessage)
